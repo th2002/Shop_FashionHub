611c1798d481fb0869831804c656ae707db2f2e2
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2023-07-12 17:46:29 +0700

    update layout customer
    update liên kết css

--- app/views/customer/layout.php
+++ app/views/customer/layout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" href="../../content/css/grid.css">
+    <link rel="stylesheet" href="../../../assets/css/grid.css">
     <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
