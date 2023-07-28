2dd790388f27a305a9fad197f5989540bc4a07ff
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2023-07-28 11:38:49 +0700

    aa

--- app/views/customer/oders/layout.php
+++ app/views/customer/oders/layout.php
@@ -80,27 +80,18 @@ $provinces = thanh_pho_select_all();
                         </div>
                     </div>
                 </div>
-
-                <select name="calc_shipping_provinces" required="">
-                    <option value="">Tỉnh / Thành phố</option>
+                <!-- code select -->
+                <select class="form-select form-select-sm mb-3" id="city" aria-label=".form-select-sm">
+                    <option value="" selected>Chọn tỉnh thành</option>
                 </select>
-                <select name="calc_shipping_district" required="">
-                    <option value="">Quận / Huyện</option>
+
+                <select class="form-select form-select-sm mb-3" id="district" aria-label=".form-select-sm">
+                    <option value="" selected>Chọn quận huyện</option>
                 </select>
-                <input class="billing_address_1" name="" type="hidden" value="">
-                <input class="billing_address_2" name="" type="hidden" value="">
 
-
-                <div class="col-md-12">
-                    <label for="validationCustom03" class="form-label">Phường / Xã</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>...</option>
-                        <option value="1">Phường 10</option>
-                        <option value="2">Phường 11</option>
-                        <option value="3">Phường 12</option>
-                    </select>
-                </div>
-
+                <select class="form-select form-select-sm" id="ward" aria-label=".form-select-sm">
+                    <option value="" selected>Chọn phường xã</option>
+                </select>
                 <div class="col-md-12">
                     <label for="validationCustom03" class="form-label">Phương thức thanh toán</label>
                     <select class="form-select" aria-label="Default select example">
@@ -213,87 +204,12 @@ $provinces = thanh_pho_select_all();
 
     </div>
 
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js' />
-    <script src='https://cdn.jsdelivr.net/gh/vietblogdao/js/districts.min.js' />
-    <script>
-    //<![CDATA[
-    if (address_2 = localStorage.getItem('address_2_saved')) {
-        $('select[name="calc_shipping_district"] option').each(function() {
-            if ($(this).text() == address_2) {
-                $(this).attr('selected', '')
-            }
-        })
-        $('input.billing_address_2').attr('value', address_2)
-    }
-    if (district = localStorage.getItem('district')) {
-        $('select[name="calc_shipping_district"]').html(district)
-        $('select[name="calc_shipping_district"]').on('change', function() {
-            var target = $(this).children('option:selected')
-            target.attr('selected', '')
-            $('select[name="calc_shipping_district"] option').not(target).removeAttr('selected')
-            address_2 = target.text()
-            $('input.billing_address_2').attr('value', address_2)
-            district = $('select[name="calc_shipping_district"]').html()
-            localStorage.setItem('district', district)
-            localStorage.setItem('address_2_saved', address_2)
-        })
-    }
-    $('select[name="calc_shipping_provinces"]').each(function() {
-        var $this = $(this),
-            stc = ''
-        c.forEach(function(i, e) {
-            e += +1
-            stc += '<option value=' + e + '>' + i + '</option>'
-            $this.html('<option value="">Tỉnh / Thành phố</option>' + stc)
-            if (address_1 = localStorage.getItem('address_1_saved')) {
-                $('select[name="calc_shipping_provinces"] option').each(function() {
-                    if ($(this).text() == address_1) {
-                        $(this).attr('selected', '')
-                    }
-                })
-                $('input.billing_address_1').attr('value', address_1)
-            }
-            $this.on('change', function(i) {
-                i = $this.children('option:selected').index() - 1
-                var str = '',
-                    r = $this.val()
-                if (r != '') {
-                    arr[i].forEach(function(el) {
-                        str += '<option value="' + el + '">' + el + '</option>'
-                        $('select[name="calc_shipping_district"]').html(
-                            '<option value="">Quận / Huyện</option>' + str)
-                    })
-                    var address_1 = $this.children('option:selected').text()
-                    var district = $('select[name="calc_shipping_district"]').html()
-                    localStorage.setItem('address_1_saved', address_1)
-                    localStorage.setItem('district', district)
-                    $('select[name="calc_shipping_district"]').on('change', function() {
-                        var target = $(this).children('option:selected')
-                        target.attr('selected', '')
-                        $('select[name="calc_shipping_district"] option').not(target)
-                            .removeAttr('selected')
-                        var address_2 = target.text()
-                        $('input.billing_address_2').attr('value', address_2)
-                        district = $('select[name="calc_shipping_district"]').html()
-                        localStorage.setItem('district', district)
-                        localStorage.setItem('address_2_saved', address_2)
-                    })
-                } else {
-                    $('select[name="calc_shipping_district"]').html(
-                        '<option value="">Quận / Huyện</option>')
-                    district = $('select[name="calc_shipping_district"]').html()
-                    localStorage.setItem('district', district)
-                    localStorage.removeItem('address_1_saved', address_1)
-                }
-            })
-        })
-    })
-    //]]>
-    </script>
 
     <script type="text/javascript" src="<?= $ASSET_URL ?>/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
     <script src="<?= $ASSET_URL ?>/js/snippets.js"></script>
     <script src="<?= $ASSET_URL ?>/js/modal.js"></script>
+    <script src="<?= $ASSET_URL ?>/js/app.js"></script>
 </body>
 
 </html>
