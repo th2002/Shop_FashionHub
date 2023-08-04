6eb977a6964a4e8471b2b48ea9a89a90a9aaa2c8
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2023-08-04 10:31:01 +0700

    update css

--- app/views/customer/page_user/index.php
+++ app/views/customer/page_user/index.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tài khoản</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" href="<?=$ASSET_URL?>/css/grid.css">
     <link rel="stylesheet" href="<?=$ASSET_URL?>/css/app.css">
